cb0e52d26dab422ca543ec12f605cbd7a5a2299b
Lev Babchenko <lev@3divi.com>
2015-05-15 10:01:40 +0500

    check_fstree

---
+++ check_fstree.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+cd "$(dirname $(readlink -f $0))"
+. wowauc.conf
+mkdir -p \
+  $dir_fetching $dir_fetched \
+  $dir_importing $dir_imported \
+  $dir_zipping $dir_zipped \
+  $dir_fetched_items $dir_importing_items $dir_imported_items \
+  $dir_zipping_items $dir_zipped_items
